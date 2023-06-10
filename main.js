bbe028869dbd83b870aae3e32b6b27225032fc60
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-06-10 17:53:27 +1000

    Add onCLickEventListener to calculate the expenses

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+// Declare calculate function expression to run it when the user clicks on the results button.
+let calculate = () => {
+    let calculateBtn = document.querySelector("#calculateBtn");
+    // Listen for click
+    calculateBtn.addEventListener('click', (e) => {
+        console.log("click");
+    });
+}
+
+
 // Display the next section and hide the current section.
 function displaySection(currentSection, nextSection) {
     let current = document.querySelector(currentSection);
@@ -6,4 +16,9 @@ function displaySection(currentSection, nextSection) {
     current.classList.add("hidden");
     // Remove hidden class from the current section.
     next.classList.remove("hidden");
+
+    // Calculate if the user clicks on the results button.
+    if (nextSection === "#expensesThree") {
+        calculate();
+    }
 }
