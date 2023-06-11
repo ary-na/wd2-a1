af16a9a840521a74809069f1a9e89bbbcee41e4f
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-06-11 19:04:38 +1000

    Add calculateNetResult function

--- main.js
+++ main.js
@@ -1,12 +1,48 @@
+// Get user budget.
+let budget = document.querySelector("#budgetInput").value;
+
+
 // Declare calculate function expression to run it when the user clicks on the results button.
 let calculate = () => {
     let calculateBtn = document.querySelector("#calculateBtn");
-    // Listen for click
+    // Listen for click.
     calculateBtn.addEventListener('click', (e) => {
-        console.log("click");
+        let total = calculateTotalExpenses();
+        calculateNetResult(total);
     });
 }
 
+// Calculate total and average expenses.
+function calculateTotalExpenses() {
+    let expensesCatOneSubtotal = calculateSubtotalPerCat("expensesOne");
+    let expensesCatTwoSubtotal = calculateSubtotalPerCat("expensesTwo");
+    let expensesCatThreeSubtotal = calculateSubtotalPerCat("expensesThree");
+    // Sum the subtotals and store the total in the total variable.
+    let total = expensesCatOneSubtotal + expensesCatTwoSubtotal + expensesCatThreeSubtotal;
+    // Divide the total by nine expenses and store the average cost per expense variable.
+    let averageCostPerExpense = total / 9;
+    return total;
+}
+
+// Calculate subtotal expenses per category.
+function calculateSubtotalPerCat(category) {
+    // Get expenses for each category.
+    let expensesCat1 = document.querySelector(`#${category} #expensesThree`).value;
+    let expensesCat2 = document.querySelector(`#${category} #expensesThree`).value;
+    let expensesCat3 = document.querySelector(`#${category} #expensesThree`).value;
+    // Sum the expenses and return the total.
+    return expensesCat1 + expensesCat2 + expensesCat3;
+}
+
+// Calculate the net result and display either happy or sad image based on the result.
+function calculateNetResult(total) {
+    let netResult = budget - total;
+    if (netResult <= 0) {
+
+    } else {
+
+    }
+}
 
 // Display the next section and hide the current section.
 function displaySection(currentSection, nextSection) {
