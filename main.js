e2f45a2a5f8a7d735819103645dec188b61e8a41
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-06-12 11:07:21 +1000

    Minor changes and fixes

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ let calculate = () => {
 
 // Calculate total and average expenses.
 function calculateTotalExpenses() {
-    let expensesCatOneSubtotal = calculateSubtotalPerCat("expensesOne");
-    let expensesCatTwoSubtotal = calculateSubtotalPerCat("expensesTwo");
-    let expensesCatThreeSubtotal = calculateSubtotalPerCat("expensesThree");
+    let expensesCatOneSubtotal = calculateSubtotalPerCat("cat1Inputs");
+    let expensesCatTwoSubtotal = calculateSubtotalPerCat("cat2Inputs");
+    let expensesCatThreeSubtotal = calculateSubtotalPerCat("cat3Inputs");
     // Sum the subtotals and store the total in the total variable.
     let total = expensesCatOneSubtotal + expensesCatTwoSubtotal + expensesCatThreeSubtotal;
     // Divide the total by nine expenses and store the average cost per expense variable.
@@ -27,9 +27,9 @@ function calculateTotalExpenses() {
 // Calculate subtotal expenses per category.
 function calculateSubtotalPerCat(category) {
     // Get expenses for each category.
-    let expensesCat1 = document.querySelector(`#${category} #expensesThree`).value;
-    let expensesCat2 = document.querySelector(`#${category} #expensesThree`).value;
-    let expensesCat3 = document.querySelector(`#${category} #expensesThree`).value;
+    let expensesCat1 = document.querySelector(`#${category} #expense1`).value;
+    let expensesCat2 = document.querySelector(`#${category} #expenses2`).value;
+    let expensesCat3 = document.querySelector(`#${category} #expenses3`).value;
     // Sum the expenses and return the total.
     return expensesCat1 + expensesCat2 + expensesCat3;
 }
